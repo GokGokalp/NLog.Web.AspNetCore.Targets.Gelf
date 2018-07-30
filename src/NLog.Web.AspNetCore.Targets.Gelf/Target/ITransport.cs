f219f5ab7a210081d2b0d0b59d7e5ed8dc8c267d
tjelcic <tomislav.jelcic@outlook.com>
2018-07-30 13:09:15 +0200

    Initial commit

--- src/NLog.Web.AspNetCore.Targets.Gelf/Target/ITransport.cs
+++ src/NLog.Web.AspNetCore.Targets.Gelf/Target/ITransport.cs
@@ -6,6 +6,6 @@ namespace NLog.Web.AspNetCore.Targets.Gelf
     public interface ITransport
     {
         string Scheme { get; }
-        void Send(IPEndPoint target, string message);
+        void Send(IPEndPoint target, string message, int? overrideMaxUdpPackageSize = null);
     }
 }
